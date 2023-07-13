2ba771f34502deade3f66542a6c294dfbf60e012
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-13 20:37:28 +0000

    feeling drowsy so morning catch up all

--- Codeal/src/Components/App.js
+++ Codeal/src/Components/App.js
@@ -2,14 +2,24 @@ import { getPost } from "../api";
 import {useEffect} from 'react'
 import { Navbar } from "./Navbar";
 import {Home} from  '../Pages/Home'
+import {Loader} from './Loader'
 function App() {
-useEffect(()=>{
+  
+  const [posts,setPosts] = useState([]);
+  const [loading, setloading] = useState(true);
+  useEffect(()=>{
 let fetchPosts = async ()=>{
   let response = await getPost();
   console.log('response',response);
+  if(response.success){
+    setPosts(response.data.post);
+  }
 }
 fetchPosts();
 },[])
+if(loading){
+  <Loader />
+}
   return (
     <div className="App">
    <Navbar/>
--- Codeal/src/Pages/Home.js
+++ Codeal/src/Pages/Home.js
@@ -1,28 +1,44 @@
 import React from "react";
 import styles from "../Styles/home.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+
+
 export const Home = () => {
   return (
-    <>
-      <div className={styles.outer}>
-      <h1>Posts</h1>
+   <>
+    
+   {/* post sectin */}
+   <section>
+    <div className={styles.container} >
+      <div className={styles.userInfo}>
+      <div >
+        <img src="https://www.disneyplusinformer.com/wp-content/uploads/2021/06/Luca-Profile-Avatars-3.png" alt="userimg" width={40}/>
+      </div>
       <div>
-        <div>
-            <img src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png" alt="" width={35}/>
-            <span className={styles.username}>Jhon</span>       
-        </div>
-        <div>
-        display post
-        </div>
-        <div>
-            <h3>like</h3>
-            <h3>comment</h3>
-        </div>
-        <div>
-            <input type="text"  placeholder="leave comments"/>
-            <input type="submit" />
-        </div>
+        <h4>username</h4>
       </div>
       </div>
-    </>
+      <div>postContent</div>
+      <div className={styles.likeAndComments}>
+        <div>
+        <FontAwesomeIcon icon={faHeart} style={{color: "#000814",}} />        </div>
+        <div>
+        <FontAwesomeIcon icon={faCommentDots} />  
+            </div>
+            </div>
+      <div>
+        <input type="text" placeholder="start typeing comments " />
+      </div>
+      <div>
+        others comments
+      </div>
+    </div>
+   
+   </section>
+   </>
+          
+   
   );
 };
