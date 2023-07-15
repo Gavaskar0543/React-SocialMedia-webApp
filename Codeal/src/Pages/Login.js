6d0636bd1b1c9768c2385c879931b661322a0d43
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-07-15 09:45:02 +0000

    react toast added

--- Codeal/src/Pages/Login.js
+++ Codeal/src/Pages/Login.js
@@ -1,10 +1,27 @@
+import React,{useState} from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const Login = () => {
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [logging,setLogging] = useState(false);
+  //notification function
+  const notify = () => toast("please enter both email and password");
+
+ const handleSubmit = (e) => {
+    e.preventDefault();
+    setLogging(true);
+   if(!email  || !password){
+    notify();
+   } 
+  }
   return (
     <div className="container  mt-5 w-25">
       <div className="container">
         <h3 className="text-warning text-center">Login</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="email">Email</label>
             <input
@@ -12,8 +29,11 @@ const Login = () => {
               className="form-control"
               id="email"
               placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
+          <ToastContainer />
           <div className="form-group">
             <label htmlFor="Password">Password</label>
             <input
@@ -21,12 +41,16 @@ const Login = () => {
               id="Password"
               className="form-control"
               placeholder="Enter password"
-              required
+              
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-warning w-100 mt-2">
-            Submit
+         <div disabled={logging}>
+         <button type="submit" className="btn btn-warning w-100 mt-2">
+            {logging ? 'loging in...' : 'log in'}
           </button>
+         </div>
         </form>
       </div>
     </div>
--- Codeal/src/index.js
+++ Codeal/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './Components';
-import { BrowserRouter as Router } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./Components";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
-    </Router>
+      
+      <App />
+      
+     </Router>
   </React.StrictMode>
 );
-
-
